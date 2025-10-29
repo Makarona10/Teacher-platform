9ea7c2ac4de9d15be9fe597b88ee7b4176e4332a
Makarona10 <ahmedneyssi@gmail.com>
2025-10-29 08:46:36 +0300

    Build -> Session status badges created

---
+++ components/badges/Finished.tsx
@@ -0,0 +1,9 @@
+const FinishedBadge = () => {
+  return (
+    <p className="text-xs p-1 px-4 rounded-full bg-red-600/60 text-white w-fit">
+      مضت
+    </p>
+  );
+};
+
+export default FinishedBadge;
---
+++ components/badges/Upcoming.tsx
@@ -0,0 +1,9 @@
+const UpcomingBadge = () => {
+  return (
+    <p className="text-xs p-1 px-4 rounded-full bg-green-600/60 text-white w-fit">
+      قادمة
+    </p>
+  );
+};
+
+export default UpcomingBadge;
