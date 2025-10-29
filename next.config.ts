65e32a06f11de403cfdeed5bf61e1abaf85a0d61
Makarona10 <ahmedneyssi@gmail.com>
2025-10-29 08:52:08 +0300

    Update -> Packages and configuration updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "upload.wikimedia.org",
+      "media0087.elcinema.com",
+    ],
+  },
 };
 
 export default nextConfig;
